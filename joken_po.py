18382f33b44096125eef23366ff721c714834e24
Julio Cesar Avelar Mio <juliomio2308@gmail.com>
2021-05-06 13:55:04 -0300

    Tornando a leitura mais simples

--- joken_po.py
+++ joken_po.py
@@ -2,7 +2,7 @@ from random import randint
 from time import sleep
 
 itens = ('Pedra', 'Papel', 'Tesoura')
-computador = randint(0, 2)
+pc = randint(0, 2)
 print('''Suas opções são as seguintes:
 \033[1;34m[ 0 ] Pedra \033[m
 \033[1;34m[ 1 ] Papel \033[m
@@ -14,11 +14,11 @@ print('\033[1;33mKEN\033[m')
 sleep(1)
 print('\033[1;33mPO\033[m')
 print('-='*16)
-print('O computador escolheu \033[1;34m{}\033[m'.format(itens[computador]))
+print('O computador escolheu \033[1;34m{}\033[m'.format(itens[pc]))
 print('O jogador escolheu \033[1;34m{}\033[m'.format(itens[jogador]))
 print('-='*16)
 
-if computador == 0: # pedra
+if pc == 0: # pedra
     if jogador == 0:
         print('\033[1;32mQUE PENA\033[m .. O jogo deu \033[1;32mEMPATE\033[m')
     elif jogador == 1:
@@ -27,7 +27,7 @@ if computador == 0: # pedra
         print('O \033[1;33mCOMPUTADOR VENCEU\033[m essa partida')
     else:
         print('PARTIDA INVÁLIDA')
-if computador == 1:
+if pc == 1:
     if jogador == 0:
         print('O  \033[1;33mCOMPUTADOR VENCEU\033[m essa partida')
     elif jogador == 1:
@@ -36,7 +36,7 @@ if computador == 1:
         print('O \033[1;34mJOGADOR VENCEU\033[m essa partida')
     else:
         print('PARTIDA INVÁLIDA')
-if computador == 2:
+if pc == 2:
     if jogador == 0:
         print('O \033[1;34mJOGADOR VENCEU\033[m essa partida')
     elif jogador == 1:
@@ -45,3 +45,4 @@ if computador == 2:
         print('\033[1;32mQUE PENA\033[m.. O jogo deu \033[1;32mEMPATE\033[m')
     else:
         print('PARTIDA INVÁLIDA .. Tente Novamente')
+
